a7b6e87785b76e296d7ff95675a012acd4077cb8
Paula Domingues <pdomingues@itba.edu.ar>
2022-09-02 16:48:01 -0300

    feat: solver first iteration

--- TP1/color_palette/app/data_structure/Solver.py
+++ TP1/color_palette/app/data_structure/Solver.py
@@ -1,6 +1,16 @@
 import numpy as np
+import random
 from data_structure.Color import Color
 from functions.fitness import fitness
+
+from methods.selection.elite_selection import elite_selection
+from methods.crossover.uniform_crossover import uniform_crossover
+
+from methods.selection.roulette_selection import roulette_selection
+
+from methods.crossover.heuristic_crossover import heuristic_crossover
+
+from methods.crossover.geometric_average_crossover import geometric_average_crossover
 
 
 class Solver:
@@ -9,6 +19,9 @@ class Solver:
         self.palette_size = len(palette)
         self.target_color = target
         self.current_palette = self.generate_palette(palette)
+        self.palette_number = 0
+        while True:
+            self.evolve_palette()
 
     def generate_palette(self, palette) -> list[Color]:
         curr_palette = []
@@ -16,3 +29,23 @@ class Solver:
             curr_palette.append(Color(palette[i], fitness(self.target_color, palette[i])))
         return curr_palette
 
+    def evolve_palette(self):
+        if self.palette_number > 100000:
+            raise RuntimeError
+        new_gen = []
+        while len(new_gen) < self.palette_size:
+            sliced = elite_selection(self.current_palette, 5, self.palette_size)
+            # sliced = roulette_selection(self.current_palette, int(self.palette_size/2))
+            offspring = geometric_average_crossover(sliced[random.randint(0, len(sliced) - 1)],
+                                                    sliced[random.randint(0, len(sliced) - 1)],
+                                                    self.target_color)
+            new_gen.append(offspring[0])
+            new_gen.append(offspring[1])
+        self.current_palette = new_gen
+        self.palette_number += 1
+        for color in new_gen:
+            print(color.fitness)
+            if color.fitness >= 0.98:
+                print("found!")
+                print(color.rgb)
+                exit(1)
--- TP1/color_palette/app/main.py
+++ TP1/color_palette/app/main.py
@@ -1,5 +1,6 @@
 from arguments.parser import parser
 import numpy as np
+import random
 
 from methods.crossover.uniform_crossover import uniform_crossover
 from methods.crossover.geometric_average_crossover import geometric_average_crossover
@@ -13,33 +14,12 @@ from data_structure.Solver import Solver
 
 def main(target, available_colors):
     solver = Solver(available_colors, target)
-    print(*solver.current_palette, sep='\n')
-    print("---------------------")
-    sliced = elite_selection(solver.current_palette, 4, solver.palette_size)
-    print(*sliced, sep='\n')
-    print("---------------------")
-    sliced = roulette_selection(solver.current_palette, 4)
-    print(*sliced, sep='\n')
-    print("---------------------")
-    sliced = deterministic_tournament_selection(solver.current_palette, 4, 3)
-    print(*sliced, sep='\n')
-    print("---------------------")
-    sliced = probabilistic_tournament_selection(solver.current_palette, 4)
-    print(*sliced, sep='\n')
-    print("---------------------")
-    offspring = heuristic_crossover(sliced[0], sliced[1], target)
-    print(*offspring, sep='\n')
-    print("---------------------")
-    offspring = geometric_average_crossover(sliced[0], sliced[1], target)
-    print(*offspring, sep='\n')
-    print("---------------------")
-    offspring = uniform_crossover(sliced[0], sliced[1], target)
-    print(*offspring, sep='\n')
-    print("---------------------")
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    avail_colors = np.array([[0.31, 0.02, 0.22], [0.01, 0.42, 0.02], [0.001, 0.92, 0.02], [0.021, 0.02, 0.02]])
+    avail_colors = []
+    for i in range(5000):
+        avail_colors.append([random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)])
     target_color = np.array([0.01, 0.02, 0.021])
     main(target_color, avail_colors)
