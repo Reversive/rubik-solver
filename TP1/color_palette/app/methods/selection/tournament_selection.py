386edf08e6b649386558ea7b6b318307d69db32e
Paula Domingues <pdomingues@itba.edu.ar>
2022-09-02 11:10:13 -0300

    fix: typo

--- TP1/color_palette/app/methods/selection/tournament_selection.py
+++ TP1/color_palette/app/methods/selection/tournament_selection.py
@@ -11,12 +11,12 @@ def deterministic_tournament_selection(palette: list[Color], result_size: int, g
 
 
 def probabilistic_tournament_selection(palette: list[Color], result_size: int) -> list[Color]:
-    treshold = random.uniform(0.5, 1)
+    threshold = random.uniform(0.5, 1)
     selected = []
     while len(selected) < result_size:
         r = random.uniform(0, 1)
         pair = np.random.choice(palette, 2)
-        if r < treshold:
+        if r < threshold:
             selected.append(max(pair, key=lambda color: color.fitness))
         else:
             selected.append(min(pair, key=lambda color: color.fitness))
