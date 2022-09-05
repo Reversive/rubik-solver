659c299b2d17002c043d868685a3303a24dc39dc
Gaston <gdonikian@itba.edu.ar>
2022-09-05 18:06:23 -0300

    Changed plotter

--- TP1/rubik_cube/plotter/plotter.py
+++ TP1/rubik_cube/plotter/plotter.py
@@ -11,6 +11,11 @@ def read_csv(file):
     border_nodes = []
     time = []
     current_line = out.readline()
+    solved_percentage = 0
+    total = 0
+    while current_line.startswith('S'):
+        total += 1
+        current_line = out.readline()
     while current_line is not None and current_line != '':
         aux = current_line.split(',')
         aux[-1] = aux[-1].strip('\n')
@@ -19,9 +24,14 @@ def read_csv(file):
         border_nodes.append(int(aux[3]))
         time.append(float(aux[4]))
         current_line = out.readline()
+        while current_line.startswith('S'):
+            total += 1
+            current_line = out.readline()
+        total += 1
+        solved_percentage += 1
 
     out.close()
-    return visited_nodes, depth, border_nodes, time
+    return visited_nodes, depth, border_nodes, time, round((solved_percentage*100)/total,2)
 
 def analyze_parsed_data(visited_nodes, depth, border_nodes, time):
     visited_nodes_mean = numpy.mean(visited_nodes) + numpy.mean(border_nodes)
@@ -36,27 +46,31 @@ def read_search_method(method_name):
     time_nodes_points = []
     time_nodes_errors = []
     for i in CASES:
-        visited_nodes, depth, border_nodes, time = read_csv('./' + method_name + '/out_' + str(i) + '.csv')
+        visited_nodes, depth, border_nodes, time,solved_percentage = read_csv('./' + method_name + '/out_' + str(i) + '.csv')
         visited_node_point, visited_node_error, time_node_point, time_node_error = analyze_parsed_data(visited_nodes, depth, border_nodes, time)
         visited_nodes_points.append(visited_node_point)
         visited_nodes_error.append(visited_node_error)
         time_nodes_points.append(time_node_point)
         time_nodes_errors.append(time_node_error)
-    return visited_nodes_points, visited_nodes_error
+    return visited_nodes_points, visited_nodes_error, time_nodes_points, time_nodes_errors, solved_percentage
 
 if __name__ == '__main__':
-    visited_nodes, visited_nodes_error = read_search_method('BFS')
-    plt.errorbar(y=visited_nodes, x=CASES, yerr=visited_nodes_error, fmt='-o')
-    visited_nodes, visited_nodes_error = read_search_method('ASTAR_DIFFCOLORS')
-    plt.errorbar(y=visited_nodes, x=CASES, yerr=visited_nodes_error, fmt='-x')
+    visited_nodes, visited_nodes_error, time_nodes, time_errors, bfs_solved_percentage = read_search_method('BFS')
+    plt.errorbar(y=time_nodes, x=CASES, yerr=time_errors, fmt='-o')
+
+    visited_nodes, visited_nodes_error, time_nodes, time_errors, astar_diff_solved_percentage = read_search_method('ASTAR_DIFFCOLORS')
+    plt.errorbar(y=time_nodes, x=CASES, yerr=time_errors, fmt='-x')
+
+    visited_nodes, visited_nodes_error, time_nodes, time_errors, astar_sman_solved_percentage = read_search_method('ASTAR_SMANHATTAN')
+    plt.errorbar(y=time_nodes, x=CASES, yerr=time_errors, fmt='-s')
+
+    visited_nodes, visited_nodes_error, time_nodes, time_errors,iddfs_solved_percentage = read_search_method('IDDFS')
+    plt.errorbar(y=time_nodes, x=CASES,yerr=time_errors, fmt='-p')
     print(visited_nodes)
-    visited_nodes, visited_nodes_error = read_search_method('ASTAR_SMANHATTAN')
-    print(visited_nodes)
-    plt.errorbar(y=visited_nodes, x=CASES, yerr=visited_nodes_error, fmt='-s')
     plt.xlabel('Cases')
     plt.yscale('log')
-    plt.ylabel('Expanded nodes')
+    plt.ylabel('time(s)')
     plt.grid()
-    plt.title('Expanded nodes vs Cases')
-    plt.legend(['BFS','ASTAR_DIFFCOLORS','ASTAR_SMANHATTAN'])
+    plt.title('Time vs Cases')
+    plt.legend(['BFS', 'ASTAR_DIFFCOLORS','ASTAR_SMANHATTAN', 'IDDFS(step = 3)'])
     plt.show()
