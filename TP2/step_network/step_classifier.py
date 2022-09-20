e53c083fd0fb69614f62ec9dd5e02d4f180dc5d0
manurodriguez <manurodriguez@itba.edu.ar>
2022-09-20 19:39:08 -0300

    feat: Simple Perceptron working

--- TP2/step_network/step_classifier.py
+++ TP2/step_network/step_classifier.py
@@ -5,15 +5,9 @@ import numpy as np
 class StepClassifier:
     def __init__(self):
         self.perceptron = Perceptron(np.sign, 2, 0.01)
-        self.perceptron.train([[1, 1, 1], [0, 1, 0], [1, 0, 0], [0, 0, 0]])
-        print(self.perceptron.test([1, 1]))
-        print(self.perceptron.test([0, 1]))
-        print(self.perceptron.test([1, 0]))
-        print(self.perceptron.test([0, 0]))
-
-        self.perceptron = Perceptron(np.sign, 2, 0.01)
-        self.perceptron.train([[1, 1, 0], [0, 1, 1], [1, 0, 1], [0, 0, 0]])
-        print(self.perceptron.test([1, 1]))
-        print(self.perceptron.test([0, 1]))
-        print(self.perceptron.test([1, 0]))
-        print(self.perceptron.test([0, 0]))
+        train_dataset = [[1, 1, 1], [0, 1, -1], [1, 0, -1], [0, 0, -1]]
+        self.perceptron.train(train_dataset)
+        print(self.perceptron.test([1, 1]) == 1)
+        print(self.perceptron.test([0, 1]) == -1)
+        print(self.perceptron.test([1, 0]) == -1)
+        print(self.perceptron.test([0, 0]) == -1)
--- TP2/utils/Perceptron.py
+++ TP2/utils/Perceptron.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+THRESHOLD = 0.00001
+MAX_ITERS = 100000
+
 
 class Perceptron:
     def __init__(self, activation_func, input_dim, learning_rate):
@@ -8,23 +11,32 @@ class Perceptron:
         self.learning_rate = learning_rate
 
     def test(self, h):
-        return self.func(np.dot(self.weights.transpose(), np.array([1] + h)))
+        return self.func(np.dot(self.weights, np.array([1] + h)))
 
-    def cuadratic_error(self, expected, result):
-        return (1 / 2) * pow(expected - result, 2)
+    def cuadratic_error(self, train_data):
+        error = 0
+        for example in train_data:
+            error += (1 / 2) * pow(example[-1] - self.test(example[:-1]), 2)
+
+        return error
 
     def train(self, train_data):
         error_min = float('inf')
         w_min = self.weights
-        for example in train_data:
-            input = example[:-1]
+        i = 0
+        np.random.seed(12345)
+        while error_min > THRESHOLD and i < MAX_ITERS:
+            example = train_data[np.random.choice(range(len(train_data)))]
+            inputs = example[:-1]
             expected_output = example[-1]
-            test_result = self.test(input)
-            delta_w = self.learning_rate * (expected_output - test_result) * np.array([1] + input)
+            test_result = self.test(inputs)
+            delta_w = self.learning_rate * (expected_output - test_result) * np.array([1] + inputs)
             self.weights += delta_w
-            error = self.cuadratic_error(expected_output, test_result)
+            error = self.cuadratic_error(train_data)
             if error < error_min:
                 error_min = error
                 w_min = self.weights
+
+            i += 1
 
         self.weights = w_min
